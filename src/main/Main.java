6a8e8e4841b4397a1130aa07b55e2fb78f3d3eac
Ama Abrampah <asabrampah@st.ug.edu.gh>
2023-08-15 16:51:06 +0000

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+//Ama Sakyiwah Abrampah
+//ID: 10896568
+
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/main.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Inventory Management System!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
